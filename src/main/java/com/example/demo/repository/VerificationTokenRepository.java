cb2205b8dbc6d6c5eaa4189dca9293736611bb18
kosta <kosta.lazarevski89@gmail.com>
2020-10-11 23:19:15 +0200

    Repositories created

--- src/main/java/com/example/demo/repository/VerificationTokenRepository.java
+++ src/main/java/com/example/demo/repository/VerificationTokenRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VerificationTokenRepository extends JpaRepository<VerificationTokenRepository, Long> {
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 }
