c2957c7494de11df5e8a1ea057c86d6a76c7e55c
kosta <kosta.lazarevski89@gmail.com>
2020-10-12 19:19:30 +0200

    RequestBody parameter fix

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.RegisterRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     @PostMapping("/sugnup")
-    public void signup(RequestBody RegisterRequest registerRequest){
+    public void signup(@RequestBody RegisterRequest registerRequest){
 
     }
 }
