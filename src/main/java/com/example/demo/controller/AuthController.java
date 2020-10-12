67d009b7f4dcd2b582e344e322077b0c6f033ede
kosta <kosta.lazarevski89@gmail.com>
2020-10-12 18:53:33 +0200

    User registration implementation

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/sugnup")
+    public void signup(RequestBody RegisterRequest registerRequest){
+
+    }
+}
