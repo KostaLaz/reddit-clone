bd96ee76c3a208f2ff6542f1888d4ae21000e086
kosta <kosta.lazarevski89@gmail.com>
2020-10-13 01:20:57 +0200

    sendMail method fix

--- src/main/java/com/example/demo/exeption/SpringRedditExeption.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.exeption;
-
-public class SpringRedditExeption extends RuntimeException {
-    public SpringRedditExeption(String s) {
-    }
-}
--- src/main/java/com/example/demo/service/MailService.java
+++ src/main/java/com/example/demo/service/MailService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.exeption.SpringRedditExeption;
+import com.example.demo.exception.SpringRedditException;
 import com.example.demo.model.NotificationEmail;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,20 +19,21 @@ public class MailService {
     private final JavaMailSender mailSender;
     private final MailContentBuilder mailContentBuilder;
 
-
-    public void sendMail(NotificationEmail notificationEmail){
+    @Async
+    void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-            messageHelper.setFrom("redditclone@email.com");
-            mimeMessage.setSubject(notificationEmail.getSubject());
-            mimeMessage.setText(mailContentBuilder.build(notificationEmail.getBody()));
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(notificationEmail.getBody());
         };
-        try{
+        try {
             mailSender.send(messagePreparator);
-            log.info("Activation emal sent.");
-        }catch (MailException e){
-            throw new SpringRedditExeption("Exeption occured when sending mail to " + notificationEmail.getRecipient());
+            log.info("Activation email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new SpringRedditException("Exception occurred when sending mail to " + notificationEmail.getRecipient(), e);
         }
-
     }
 }
