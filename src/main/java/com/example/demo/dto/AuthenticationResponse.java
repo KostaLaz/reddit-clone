d36fe23cd7fb8e096d36f505245d87a37fc744ac
kosta <kosta.lazarevski89@gmail.com>
2020-10-24 23:11:09 +0200

    typo fixed

--- src/main/java/com/example/demo/dto/AuthenticationResponse.java
+++ src/main/java/com/example/demo/dto/AuthenticationResponse.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthenticationResponse {
 
-    private String authentikationToken;
+    private String authenticationToken;
     private String username;
 }
