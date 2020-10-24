a0ead5d7954aef173bdf0969917742e327aa91c2
kosta <kosta.lazarevski89@gmail.com>
2020-10-24 18:48:53 +0200

    jwt provider service logic implemented

---
+++ src/main/java/com/example/demo/security/JwtProvider.java
@@ -0,0 +1,17 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtProvider {
+    public String generateToken(Authentication authentication){
+        User principal = (User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .compact();
+    }
+}
