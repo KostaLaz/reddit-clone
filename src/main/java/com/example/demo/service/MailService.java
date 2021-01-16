cdcc3c7a44e3c627c23af6edd0f0d9bdf92d4c57
KostaLaz <kosta.lazarevski89@gmail.com>
2021-01-16 19:04:11 +0100

    MailService - send activation mail logic

---
+++ src/main/java/com/example/demo/service/MailService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    public void sendEmail(NotificationEmail notificationEmail){
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setFrom("example@email.com");
+            mimeMessageHelper.setTo(notificationEmail.getRecipient());
+            mimeMessageHelper.setSubject(notificationEmail.getSubject());
+            mimeMessageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+        try {
+            mailSender.send(messagePreparator);
+            log.("Activation email sent.");
+        }catch (MailException e){
+            log.("email not sent.");
+            e.printStackTrace();
+        }
+    }
+
+
+}
