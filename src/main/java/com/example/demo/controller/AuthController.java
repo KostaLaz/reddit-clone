6a6f1efa8d0d15b895a4baec72e950bea6723cba
KostaLaz <kosta.lazarevski89@gmail.com>
2021-01-18 23:27:04 +0100

    AuthController changes made

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.RegisterRequest;
+import com.example.demo.service.AuthService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
 
     @PostMapping("/signup")
     public void signup(@RequestBody RegisterRequest registerRequest){
