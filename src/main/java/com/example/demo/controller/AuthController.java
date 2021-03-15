d338fe3642dd6b1f5dc35e82834897bb29e7d3da
KostaLaz <kosta.lazarevski89@gmail.com>
2021-03-15 21:05:15 +0100

    verifyAccount endpoint

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.RegisterRequest;
-import com.example.demo.model.User;
-import com.example.demo.service.AuthService;
-import com.example.demo.service.serviceimpl.AuthServiceImpl;
+import com.example.demo.service.AuthServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthServiceImpl authService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
         authService.signup(registerRequest);
         return new ResponseEntity<>("User registration successfull", HttpStatus.OK);
+    }
+
+    @GetMapping("/accountVerification?{token}")
+    public ResponseEntity<String> verifyAccoubt(@PathVariable String token){
+        authService.verfyAccount(token);
     }
 }
