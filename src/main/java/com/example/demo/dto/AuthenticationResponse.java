4736c6f38abf2ca475f28502dbebc635cec269da
KostaLaz <kosta.lazarevski89@gmail.com>
2021-03-21 16:00:33 +0100

    AuthenticationResponse dto

--- src/main/java/com/example/demo/dto/AuthenticationResponse.java
+++ src/main/java/com/example/demo/dto/AuthenticationResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationTokenDto {
+public class AuthenticationResponse {
     private String authenticationToken;
     private String username;
 }
